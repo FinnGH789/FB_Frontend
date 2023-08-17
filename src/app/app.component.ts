74b0576f1182c435cc09b08c8ecd8b3b25c56e43
Finnb <finnb@freenet.de>
2023-08-17 21:31:01 +0200

    rxjs, observavles eingebaut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatenServiceService } from './daten-service.service';
+import { Person } from './person';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'FB_Frontend';
+
+  person: Person[] = [];
+
+  constructor(private datenService : DatenServiceService){ }
+
+  ngOnInit(): void {
+       this.datenService.getPersonsList().subscribe(data => {
+        console.log(data);
+      });
+  }
+
+  
+
+  
+
 }
--- src/app/daten-service.service.ts
+++ src/app/daten-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Person } from './person';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatenServiceService {
 
-  constructor() { }
+  private baseUrl = "http://localhost:8080/getPersons";
+
+  constructor(private httpClient: HttpClient) {  }
+
+  getPersonsList(): Observable<Person[]>{
+    return this.httpClient.get<Person[]>(`${this.baseUrl}`)
+  }
 }
